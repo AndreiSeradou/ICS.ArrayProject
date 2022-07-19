0d39a4924a7e49a6305a72949330d4699f06e133
Saydamov Sodiq <saydamov1997@mail.ru>
2022-07-19 15:05:52 +0300

    Добавьте файлы проекта.

---
+++ ICS.ArrayProject/CustomArray.cs
@@ -0,0 +1,55 @@
+﻿namespace ICS.ArrayProject
+{
+    public static class CustomArray
+    {
+        public static int[] СonvertArrays(int[] firstArray, int[] sercondArray)
+        {
+            int[] result = new int[sercondArray.Length];
+            List<int> listOfSumSergents = new List<int>();
+            int sumSegment = default;
+            bool isFound = false;
+
+            for (int i = 0; i < sercondArray.Length; i++)
+            {
+                for (int j = 0; j < firstArray.Length; j++)
+                {
+                    if (sercondArray[i] <= firstArray[j] && isFound == false)
+                    {
+                        isFound = true;
+                        sumSegment += firstArray[j];
+                    }
+                    else if (isFound == true)
+                    {
+                        sumSegment += firstArray[j];
+                    }
+                    if ((sercondArray[i] > firstArray[j] && isFound == true) || (j + 1 == firstArray.Length && isFound == true))
+                    {
+                        listOfSumSergents.Add(sumSegment);
+                        sumSegment = default;
+                        isFound = false;
+                    }
+                }
+
+                result[i] = GetMaxNumberInList(listOfSumSergents);
+                listOfSumSergents.Clear();
+            }
+
+            return result;
+        }
+        
+        private static int GetMaxNumberInList(List<int> ints)
+        {
+            int result = ints.FirstOrDefault();
+
+            foreach (int i in ints)
+            {
+                if (result < i)
+                {
+                    result = i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ICS.ArrayProject/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ICS.ArrayProject;
+
+var firstArray = new int[] { 8,5,5, 5, 3, 5, 14 };
+var secondArray = new int[] { 5 };
+
+var result = CustomArray.СonvertArrays(firstArray, secondArray);
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine($"result {i + 1}: {result[i]}");
+}
+
+Console.ReadLine();
